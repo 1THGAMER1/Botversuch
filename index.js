93d3c904430f68e6f6e55cdbb6d244b2a0ccd86b
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-09 15:58:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.commands = new Discord.Collection();
 
 bot.on('ready', () => {
     console.log(`${bot.user.username} is online `)
-    bot.user.setActivity("ocean!invite", {type: "PLAYING"});
+    bot.user.setActivity("JETZT 24/7 ONLINE", {type: "PLAYING"});
 
 })
 
