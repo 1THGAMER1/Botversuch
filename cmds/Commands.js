7c8b15ccd9b0ffa29e26baf6e071e80be39e9d5f
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-09 21:39:06 +0200

    Update Commands.js

--- cmds/Commands.js
+++ cmds/Commands.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot,message,args) => {
     const command = new MessageEmbed()
     .setTitle('Commands')
     .setColor(0x125DFF)
-    .addField('Das sind meine Befehle:', 'UNTER ARBEIT', true)
+    .addField('Das sind meine Befehle:', 'ocean!dog = Schickt ein Random Hunde Bild! /n ocean!cat = Schickt ein Random Katzen Bild /n ocean!meme = schickt ein Random Meme /n ocean!embed = Lasse deinen eigenen Comman erstellen! /n /n BOT IST NOCH UNTER ARBEIT', true)
     .setFooter('Support: Ocean TH GAMER#4958')
     message.channel.send(command)
 }
