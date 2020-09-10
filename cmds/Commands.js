31fe1422b38a36419dd2a241653a17ed216d482a
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-10 14:42:15 +0200

    Update Commands.js

--- cmds/Commands.js
+++ cmds/Commands.js
@@ -1,4 +1,5 @@
-const { MessageEmbed } = require('discord.js');
+
+const { MessageEmbed, Invite } = require('discord.js');
 
 
 
@@ -6,10 +7,12 @@ module.exports.run = async (bot,message,args) => {
     message.delete();
 
     const command = new MessageEmbed()
-    .setTitle('Commands')
+    .setTitle('Ocean Bot [Public] Commands')
     .setColor(0x125DFF)
-    .addField('Das sind meine Befehle:', 'ocean!dog = Schickt ein Random Hunde Bild! \n ocean!cat = Schickt ein Random Katzen Bild \n ocean!meme = schickt ein Random Meme \n ocean!embed = Lasse deinen eigenen Comman erstellen! \n \n BOT IST NOCH UNTER ARBEIT', true)
+    .addField('Das sind meine Befehle:', '```ocean!dog = Schickt ein Random Hunde Bild!``` \n ```ocean!cat = Schickt ein Random Katzen Bild```  \n ```ocean!meme = schickt ein Random Meme```  \n ```ocean!embed = Lasse deinen eigenen Command erstellen!```  \n \n ``BOT IST NOCH UNTER ARBEIT | MEHR COMMANDS FOLGEN NOCH! ``', true)
     .setFooter('Support: Ocean TH GAMER#4958')
+
+    
     message.channel.send(command)
 }
 
