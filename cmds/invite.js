2370ade6985a63fe855b960ecae631dcffee4981
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-10 14:42:44 +0200

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -1,9 +1,17 @@
+const { MessageEmbed } = require('discord.js');
 
 
 module.exports.run = async (bot,message,args) => {
     message.delete();
-    message.channel.send("Mein Einladungs Link: https://discord.com/oauth2/authorize?client_id=711568834590408765&scope=bot&permissions=8.");
 
+
+    const Invite = new MessageEmbed()
+    .setTitle('Mein Einladungslink')
+    .setColor(0x125DFF)
+    .setFooter('Support: Ocean TH GAMER#4958')
+    .setURL('https://discord.com/oauth2/authorize?client_id=711568834590408765&scope=bot&permissions=8')
+
+    message.channel.send(Invite)
 
 }
 
