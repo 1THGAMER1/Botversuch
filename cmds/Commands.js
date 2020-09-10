5e33457a3b679831a9eee641bb50427053dbc1b6
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-10 21:01:34 +0200

    Update Commands.js

--- cmds/Commands.js
+++ cmds/Commands.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot,message,args) => {
     const command = new MessageEmbed()
     .setTitle('Ocean Bot [Public] Commands')
     .setColor(0x125DFF)
-    .addField('Das sind meine Befehle:', '```ocean!dog = Schickt ein Random Hunde Bild!``` \n ```ocean!cat = Schickt ein Random Katzen Bild```  \n ```ocean!meme = schickt ein Random Meme```  \n ```ocean!embed = Lasse deinen eigenen Command erstellen!```  \n \n ``BOT IST NOCH UNTER ARBEIT | MEHR COMMANDS FOLGEN NOCH! ``', true)
+    .addField('Das sind meine Befehle:', '```ocean!dog = Schickt ein Random Hunde Bild!``` ```ocean!cat = Schickt ein Random Katzen Bild``` ```ocean!meme = schickt ein Random Meme``` ```ocean!stats lifetime {DEIN EPICNAME} = Zeigt deine Fortnitestats an. **Der Command funktioniert zurzeit nur mit Epic Namen ohne Leerzeichen!``` ```ocean!embed = Lasse deinen eigenen Command erstellen!```  \n ``BOT IST NOCH UNTER ARBEIT | MEHR COMMANDS FOLGEN NOCH! ``', true)
     .setFooter('Support: Ocean TH GAMER#4958')
 
     
