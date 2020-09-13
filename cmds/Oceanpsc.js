02413f88349b76067274c7f1327152ec660fea9b
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-13 14:10:36 +0200

    Update Oceanpsc.js

--- cmds/Oceanpsc.js
+++ cmds/Oceanpsc.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot,message,args) => {
     const psc = new MessageEmbed()
     .setTitle('Paysafecard Verlosung!')
     .setColor(0x125DFF)
-    .addField('So machst du bei der Verlosung mit', '1. Joine dem Ocean Clan, Pro Team oder anderen Team von Ocean \n 2. Nachdem du dann im Clan angenommen worden bist schaltest du die Clan Chats frei; eins davon heißt #10€-psc-verlosung. 3. In diesem Chat musst du dann die vom Bot vorgegebene Zahl schreiben. 4. Jetzt machst du schon bei der Verlosung mit! , true)
+    .addField('So machst du bei der Verlosung mit', '1. Joine dem Ocean Clan, Pro Team oder anderen Team von Ocean \n 2. Nachdem du dann im Clan angenommen worden bist schaltest du die Clan Chats frei; eins davon heißt #10€-psc-verlosung. 3. In diesem Chat musst du dann die vom Bot vorgegebene Zahl schreiben. 4. Jetzt machst du schon bei der Verlosung mit!' , true)
     .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/754673270602858497/1AA.jpg')
     .setFooter('Schreibe für Fragen Ocean TH GAMER#4958 an')
     message.channel.send(psc);
