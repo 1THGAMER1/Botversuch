3295759d13d7729a7dc75945f588e258e094eb09
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-15 18:49:43 +0200

    Update nzahl.js

--- cmds/nzahl.js
+++ cmds/nzahl.js
@@ -3,7 +3,7 @@
 module.exports.run = async (bot,message,args) => {
 message.delete()
     
-    message.channel.send("Nächste Zahl ist 14 \n \n ``Eingereichte Clips: Ocean NUKL3ARXx (noch 0 Zahlen übrig) \n Ocean Ishmam (noch 2 Zahlen übrig)``");
+    message.channel.send("Nächste Zahl ist 14 \n \n ``Eingereichte Clips: Ocean NUKL3ARXx (noch 0 Zahlen übrig) \n Ocean Ishmam (noch 1 Zahl übrig)``");
 
 
 }
