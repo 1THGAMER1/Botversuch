5f81e1de3bdc633cc9c34e8a2de603542edb5284
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-16 16:46:39 +0200

    Update oceanHOWTOJOIN.js

--- cmds/oceanHOWTOJOIN.js
+++ cmds/oceanHOWTOJOIN.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot,message,args) => {
     const htj = new MessageEmbed()
     .setTitle('So joinst du Team Ocean!')
     .setColor(0x125DFF)
-    .addField('', 'Lies dir lies dir zuerst #╔-proteam-infos☠ durch und wenn du die Anforderungen nicht erfüllen solltest #╠-clan-infos-📌' , true)
+    .addField(' Ocean Clan und Pro Team ', 'Lies dir zuerst #╔-proteam-infos☠ durch und wenn du die Anforderungen nicht erfüllen solltest #╠-clan-infos-📌' , true)
     .setFooter('Schreibe für Fragen das Management von Team Ocean an')
     message.channel.send(htj);
 }
