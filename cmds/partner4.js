be53241bc2d4b2024a3795533d9bf7cec6d224f1
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-20 12:40:01 +0200

    Update partner4.js

--- cmds/partner4.js
+++ cmds/partner4.js
@@ -1,1 +1,21 @@
 
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async (bot,message,args) => {
+message.delete()
+
+const partner4 = new MessageEmbed()
+.setTitle('Team Ocean Partnerschaft')
+.setColor(0x125DFF)
+.addField('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n ◥◣◆◢◤ Austria Pro Clan ◥◣◆◢◤ \n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', ' ◆ Ansprechsperson: <@584765587213778985> \n ◆ Discord: https://discord.gg/XBQepMz')
+.setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
+.setTimestamp()
+.setFooter('Schreibe für weitere Fragen unser Management an!')
+
+message.channel.send(partner4)
+
+}
+
+module.exports.help = {
+    name: 'partner4'
+}
