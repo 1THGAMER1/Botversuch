150dfb0eaf0df48d6a5aa70633024d4371de0752
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-27 14:14:25 +0200

    Update oceanclan.js

--- cmds/oceanclan.js
+++ cmds/oceanclan.js
@@ -12,8 +12,8 @@ module.exports.run = async (bot,message,args) => {
     .setColor(0x125DFF)
     .addField('Um in unseren Clan zu kommen, musst du folgendes tun:', '• Du musst deinen Epic-Namen in „Ocean Name“ ändern. \n \n Bsp. Ocean Anni \n \n • Dann ein Bild in den Channel #bewerbung✏ rein schicken, wie du deinen Namen geändert hast. \n \n • Danach einen Clan-Manager kontaktieren, der dich dann bei Gelegenheit aufnehmen wird.', true)
     .setDescription('Jeder darf in den normalen Clan!')
-    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
-    .setFooter('Developed by Ocean TH GAMER')
+    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
+    .setFooter('Bei weiteren Fragen einfach bei einem Clanmanager melden')
     message.channel.send(clan);
 }
 
