1c4ab5fa40d179dc736b4dc046936c3dbd2e880b
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-09-27 14:10:39 +0200

    Update Oceanregeln.js

--- cmds/Oceanregeln.js
+++ cmds/Oceanregeln.js
@@ -10,8 +10,8 @@ module.exports.run = async (bot,message,args) => {
     .setColor(0x125DFF)
     .addField('OCEAN CLAN', '🚫 Beleidigung jeglicher Art ist untersagt. \n \n 🚫 Werbung für Clans etc. Ist verboten. \n \n 🚫Sonstige Werbung ist nur in #eigenwerbung erlaubt \n \n 🚫 Sämtliche Werbung und Spam ist untersagt.  \n \n 🚫 Sämtlicher Handel (Accounts, Guthabenkarten etc.) und Betteln (Skins, V-Bucks etc.) ist auf diesen Server untersagt. \n \n 🚫 Jugendgefährdende Themen sind aufgrund des hohen Anteils an Minderjährigen auf diesem Server verboten.', true)
     .setDescription('Gut  durchlesen')
-    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
-    .setFooter('Develeoped by Ocean TH GAMER')
+    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
+    .setFooter('Weitere Fragen? Frag unser Management!')
 
     message.channel.send(regeln)
 }
