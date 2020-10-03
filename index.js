efce1d3132ee266c927844755a9db4c6e0cba2ce
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-03 11:25:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.commands = new Discord.Collection();
 
 bot.on('ready', () => {
     console.log(`${bot.user.username} is online `)
-    bot.user.setActivity("Joine jetzt Team Ocean!", {type: "PLAYING"});
+    bot.user.setActivity("Coding Team Ocean Website", {type: "PLAYING"});
 
 })
 
