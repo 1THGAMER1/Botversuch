2582fc070462d9fe091cb58ae5fb0b8b68a098ff
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-07 15:09:48 +0200

    Update teamocean.js

--- cmds/teamocean.js
+++ cmds/teamocean.js
@@ -7,7 +7,7 @@ module.exports.run = async(bot, message, args) => {
 
     const Anforderungen = new MessageEmbed()
     .setTitle('Team Ocean Beitritt')
-    .addField('Um unserem Clan beizutreten musst du folgende Grundvoraussetzungen erfüllen: \n ▫ bereit sein **den Epic Namen** zu ändern.  \n ▫ Höfliches und nicht toxisches Verhalten. \n \n __Anforderungen__', '<@&6730918775657857056> \n ▫ Top 1% in Cups \n ▫ 6000 Arena Punkte \n \n <@&730764354835841044> \n ▫ Top 10% in Cups \n ▫ 5500 Arena Punkte \n \n <@&730917730848342077> \n ▫ Top 20% in Cups \n ▫ 4500 Arena Punkte \n \n <@&730762306581168159> \n ▫ Das ist unser Community Clan! Jeder darf beitreten!', true)
+    .addField('Um unserem Clan beizutreten musst du folgende Grundvoraussetzungen erfüllen: \n ▫ bereit sein **den Epic Namen** zu ändern.  \n ▫ Höfliches und nicht toxisches Verhalten. \n \n __Anforderungen__', '<@&730918775657857056> \n ▫ Top 1% in Cups \n ▫ 6000 Arena Punkte \n \n <@&730764354835841044> \n ▫ Top 10% in Cups \n ▫ 5500 Arena Punkte \n \n <@&730917730848342077> \n ▫ Top 20% in Cups \n ▫ 4500 Arena Punkte \n \n <@&730762306581168159> \n ▫ Das ist unser Community Clan! Jeder darf beitreten!', true)
     .setColor(0x125DFF)
     .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
     message.channel.send(Anforderungen)
