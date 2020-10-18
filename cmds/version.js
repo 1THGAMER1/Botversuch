d6822c77b2128e30d83d5c4f0b888a064a6c4a08
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-18 15:02:57 +0200

    Update version.js

--- cmds/version.js
+++ cmds/version.js
@@ -2,7 +2,7 @@
 
 module.exports.run = async (bot,message,args) => {
     message.delete()
-    message.channel.send("Bot befindet sich in Version 0.7.1 \n\n **Neues bei dieser Version (0.7.1) ** \n Du willst wissen wie du Team Ocean joinst? Schreibe ocean!join \n \n **Überarbeitet (0.7.1)** Verschiednene Korrekturen für jeden Command");
+    message.channel.send("Bot befindet sich in Version 0.7.3 \n\n **Überarbeitet (0.7.3)** Verschiednene Korrekturen und Überarbeitungen für jeden Command");
 }
 
 module.exports.help = {
