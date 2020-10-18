8031ff9362eaf66c27ff53b8721a5faf609a2e1a
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-18 15:03:33 +0200

    Update oceangremium.js

--- cmds/oceangremium.js
+++ cmds/oceangremium.js
@@ -9,8 +9,8 @@ module.exports.run = async (bot,message,args) => {
     .setColor(0x125DFF)
     .addField('Gremium', 'Im gremium klären Wir Mod Bewerbungen ab heißt: Wenn einer eine Bewerbung als mod, Supporter etc. geschickt hat klären wir hier ab ob er/sie den Rang bekommt. Falls jemand gebannt werden sollte klären wir es auch hier ab. Für weitere Fragen einfach im Gremium fragen.', true)
     .setDescription('Alles was im Gremium besprochen wird, bleibt im Gremium!')
-    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
-    .setFooter('Develeoped by Ocean TH GAMER')
+    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
+    .setFooter('Wende dich bei weiteren Fragen gerne bei Ocean TH GAMER')
 
     message.channel.send(Gremium)
 }
