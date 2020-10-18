695bf31ae11a50cfc2fae7b94abea3b753207cc1
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-18 15:02:13 +0200

    Update oceansocialmedia.js

--- cmds/oceansocialmedia.js
+++ cmds/oceansocialmedia.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot,message,args) => {
     .setColor(0x125DFF)
     .addField('Hier unsere Social Media', ' Insta: https://instagram.com/_team_ocean_?igshid=1gthnsd4q9vj \n \n YouTube: https://www.youtube.com/channel/UCilPwqa1UF3NXS6bDuwHnzg?view_as=subscriber \n \n Twitch: https://twitch.tv/ocean_clan01 \n \n Twitter:  https://twitter.com/TeamOeanFN?s=09 \n \n TikTok: Bald', true)
     .setDescription('Es würde uns freuen wenn du uns supportest indem du uns folgst!')
-    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
+    .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
     .setFooter('Developed by Ocean TH GAMER')
 
     message.channel.send(socialmedia)
