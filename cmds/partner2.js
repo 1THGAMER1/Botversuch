4fc092b5a25288543b5b7b95c88c1636a74f89ea
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-23 13:44:57 +0200

    Update partner2.js

--- cmds/partner2.js
+++ cmds/partner2.js
@@ -8,7 +8,7 @@ const partner2 = new MessageEmbed()
 .setColor(0x125DFF)
 .addField('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n ◥◣◆◢◤ Team Quarter ◥◣◆◢◤ \n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', ' ◆ Ansprechsperson: <@700332737885765632> \n ◆ Discord: https://discord.gg/nvx9kmw')
 .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
-.setFooter('Du willst auch Parner werden? Schreibe das Management an!')
+ .setFooter(`Team Ocean Partner`, 'https://cdn.discordapp.com/attachments/730948104307867679/761238910931304459/favicon-96x96.png');
 
 message.channel.send(partner2)
 
