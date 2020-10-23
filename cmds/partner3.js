52e25404fed8158cf1e8796a244a2ef8c2063e76
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-23 13:47:22 +0200

    Update partner3.js

--- cmds/partner3.js
+++ cmds/partner3.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
 .setTitle('Team Ocean Partnerschaft')
 .setColor(0x125DFF)
 .addField('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n ◥◣◆◢◤ TEAM KODYX ◥◣◆◢◤ \n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', ' ◆ Ansprechsperson: <@488664221513678848> \n ◆ Discord: https://discord.gg/8vb4Rr3')
-.setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
-. .setFooter(`Team Ocean`, 'https://cdn.discordapp.com/attachments/730948104307867679/761238910931304459/favicon-96x96.png');
+.setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/758353626845544458/20200712_180707-1.png')
+. .setFooter(`Team Ocean Partnerschaft`, 'https://cdn.discordapp.com/attachments/730948104307867679/761238910931304459/favicon-96x96.png');
 
 message.channel.send(partner3)
 
