165490ebab36701723312cc3fd46a779c12bf092
1THGAMER1 <65722663+1THGAMER1@users.noreply.github.com>
2020-10-25 10:13:02 +0100

    Update partner5.js

--- cmds/partner5.js
+++ cmds/partner5.js
@@ -10,7 +10,7 @@ const partner5 = new MessageEmbed()
 .addField('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \n ◥◣◆◢◤ Team Turtle ◥◣◆◢◤ \n ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬', ' ◆ Ansprechsperson: <@518453030383779861> \n ◆ Discord: https://discord.gg/CE558sv')
 .setThumbnail('https://cdn.discordapp.com/attachments/730948104307867679/752123567340126319/20200906_131017.gif')
 .setTimestamp()
- .setFooter(`Team Ocean Partnerschaft`, 'https://cdn.discordapp.com/attachments/730948104307867679/761238910931304459/favicon-96x96.png');
+.setFooter(`Team Ocean Partnerschaft`, 'https://cdn.discordapp.com/attachments/730948104307867679/761238910931304459/favicon-96x96.png');
 
 message.channel.send(partner5)
 
